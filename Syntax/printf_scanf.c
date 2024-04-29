311167cd9d26b1bc142bf3cbfbb2064c0c0a7ccd
Ho Nyeong <dev.honing@gmail.com>
2024-04-29 23:13:27 +0900

    Learn: 주석을 활용한 코드 비활성화

--- Syntax/printf_scanf.c
+++ Syntax/printf_scanf.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int main(void) {
+	/* 
+	* 여러 줄 주석
 	// printf
 	printf("Hello, World!\n");
 
 	int birth = 1997;
 	printf("출생 연도는 %d\n", birth);
+	
+	*/
+	printf("1\n");
+	// printf("2\n"); 한 줄 주석
+	printf("3\n");
 
 	return 0;
 }
