573c69cc373ce44dd1354e7b4d3eeec000f8416c
Ho Nyeong <dev.honing@gmail.com>
2024-05-02 22:41:29 +0900

    Learn: 나이에 따라 분기처리
    - 20세를 기준으로 일반인/학생을 구분

--- Syntax/condition.c
+++ Syntax/condition.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("테스트다");
+	// 버스 탑승객을 학생과 일반인으로 구분한다.
+	// 일반인의 기준은 20세 이상이다.
+	
+	// 버스 탑승객 프로그램 작성
+	printf("당신의 나이를 입력하세요.");
+	int age;
+	scanf_s("%d", &age);
+
+	if (age >= 20) {
+		printf("당신은 일반인입니다.\n");
+	}
+	else {
+		printf("당신은 학생입니다.\n");
+	}
+
 
 	return 0;
 }
